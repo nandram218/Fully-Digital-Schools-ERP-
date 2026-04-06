9e86443952b8bf872eeae3c81baa10a61d52768d
nandram218 <akshaypratapsaini59@gmail.com>
2026-04-06 17:00:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { startTransition } from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>My ERP System 🚀</h1>
+      <h2>Welcome Ramnivas</h2>
+    </div>
+  );
+}
+
+export default App;
